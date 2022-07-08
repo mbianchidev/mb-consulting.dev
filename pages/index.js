0209849dd84813ee5d431d6f4b42eb17acb66d7d
mbianchidev <m.bianchi.dev@gmail.com>
2022-07-08 19:41:07 +0200

    removed redundant sections in profile, my github is not ready (yet)

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import FavouriteProjects from "@components/FavouriteProjects";
 import LatestCode from "@components/LatestCode";
 import Hero from "@components/Hero";
 import Cookies from "@components/Cookies";
-import getLatestRepos from "@lib/getLatestRepos";
-import userData from "@constants/data";
+//import getLatestRepos from "@lib/getLatestRepos";
+//import userData from "@constants/data";
 
 export default function Home({ repositories }) {
   return (
@@ -13,21 +13,26 @@ export default function Home({ repositories }) {
       description="This is my professional website. I am a developer, devops, SRE and coach."
     >
       <Hero />
+      {/* 
+      // Temporary disabled because my github is too poor 
       <FavouriteProjects />
       <LatestCode repositories={repositories} />
+      */}
       <Cookies/>
     </ContainerBlock>
   );
 }
 
-export const getServerSideProps = async () => {
-  let token = process.env.GITHUB_AUTH_TOKEN;
+// Temporary disabled because my github is too poor 
 
-  const repositories = await getLatestRepos(userData, token);
-
-  return {
-    props: {
-      repositories,
-    },
-  };
-};
+//export const getServerSideProps = async () => {
+//  let token = process.env.GITHUB_AUTH_TOKEN;
+//
+//  const repositories = await getLatestRepos(userData, token);
+//
+//  return {
+//    props: {
+//      repositories,
+//    },
+//  };
+//};
