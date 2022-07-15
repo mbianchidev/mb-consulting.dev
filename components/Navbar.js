b08d9d329d15206b699a8d33885a28fced833a1b
mbianchidev <m.bianchi.dev@gmail.com>
2022-07-15 14:15:50 +0200

    Fixed fill-rule invalid error

--- components/Navbar.js
+++ components/Navbar.js
@@ -118,7 +118,7 @@ export default function Navbar() {
                         viewBox="0 0 16 16"
                       >
                         <path 
-                          fill-rule="evenodd" d="M14 13.5a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1 0-1h4.793L2.146 2.854a.5.5 0 1 1 .708-.708L13 12.293V7.5a.5.5 0 0 1 1 0v6z"
+                          fillRule="evenodd" d="M14 13.5a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1 0-1h4.793L2.146 2.854a.5.5 0 1 1 .708-.708L13 12.293V7.5a.5.5 0 0 1 1 0v6z"
                         />
                       </svg>
                     )} Discover 
@@ -142,7 +142,7 @@ export default function Navbar() {
                         viewBox="0 0 16 16"
                       >
                         <path 
-                          fill-rule="evenodd" d="M14 13.5a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1 0-1h4.793L2.146 2.854a.5.5 0 1 1 .708-.708L13 12.293V7.5a.5.5 0 0 1 1 0v6z"
+                          fillRule="evenodd" d="M14 13.5a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1 0-1h4.793L2.146 2.854a.5.5 0 1 1 .708-.708L13 12.293V7.5a.5.5 0 0 1 1 0v6z"
                         />
                       </svg>
                     )} DevOps & Cloud 
@@ -166,7 +166,7 @@ export default function Navbar() {
                         viewBox="0 0 16 16"
                       >
                         <path 
-                          fill-rule="evenodd" d="M14 13.5a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1 0-1h4.793L2.146 2.854a.5.5 0 1 1 .708-.708L13 12.293V7.5a.5.5 0 0 1 1 0v6z"
+                          fillRule="evenodd" d="M14 13.5a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1 0-1h4.793L2.146 2.854a.5.5 0 1 1 .708-.708L13 12.293V7.5a.5.5 0 0 1 1 0v6z"
                         />
                       </svg>
                     )} Site Reliability 
@@ -190,7 +190,7 @@ export default function Navbar() {
                         viewBox="0 0 16 16"
                       >
                         <path 
-                          fill-rule="evenodd" d="M14 13.5a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1 0-1h4.793L2.146 2.854a.5.5 0 1 1 .708-.708L13 12.293V7.5a.5.5 0 0 1 1 0v6z"
+                          fillRule="evenodd" d="M14 13.5a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1 0-1h4.793L2.146 2.854a.5.5 0 1 1 .708-.708L13 12.293V7.5a.5.5 0 0 1 1 0v6z"
                         />
                       </svg>
                     )} Coaching and Courses 
@@ -214,7 +214,7 @@ export default function Navbar() {
                         viewBox="0 0 16 16"
                       >
                         <path 
-                          fill-rule="evenodd" d="M14 13.5a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1 0-1h4.793L2.146 2.854a.5.5 0 1 1 .708-.708L13 12.293V7.5a.5.5 0 0 1 1 0v6z"
+                          fillRule="evenodd" d="M14 13.5a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1 0-1h4.793L2.146 2.854a.5.5 0 1 1 .708-.708L13 12.293V7.5a.5.5 0 0 1 1 0v6z"
                         />
                       </svg>
                     )} Software Engineering 
