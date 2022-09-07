aee8232e0ea25630c05d22162ff9bc7f8bab71b5
mbianchidev <m.bianchi.dev@gmail.com>
2022-09-07 18:41:12 +0200

    Edited services page, removed eslint

--- components/Services.js
+++ components/Services.js
@@ -20,7 +20,7 @@ export default function Services(props) {
   <section className="bg-white dark:bg-gray-800">
     <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
       <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
-        Service
+        Discover Services
       </h1>
     </div>
     <br></br>
