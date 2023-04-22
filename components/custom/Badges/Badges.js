cbbc2ab9f5a6aa25f77c1ff09160f7008cf39452
Matteo Bianchi <m.bianchi.dev@gmail.com>
2023-04-22 13:16:53 +0200

    Added badge

---
+++ components/custom/Badges/Badges.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { WebsiteCarbonBadge } from 'react-websitecarbon-badge';
+
+export default function Badges() {
+
+  return (
+    <section className="bg-[#F1F1F1] -mt-40 dark:bg-gray-900 pb-40">
+      <div className="max-w-6xl mx-auto">
+        <div className="flex flex-col md:flex-row justify-between items-center md:pt-40 mx-10">
+          <h1 className="text-6xl lg:text-9xl max-w-lg font-bold text-gray-600 my-20 md:my-0 dark:text-gray-300 text-center lg:text-left">
+            Badges
+          </h1>
+
+          <WebsiteCarbonBadge dark={true} co2="0.12" percentage="89" />
+
+        </div>
+      </div>
+    </section>
+  );
+}
